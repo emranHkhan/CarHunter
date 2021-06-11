5e5e783b8f4a1d4ea5ca630db489067f98b95932
Emran H Khan <emran.khan2400@gmail.com>
2021-06-11 20:48:17 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+
+# CAR HUNTER
+
+Car Hunter is a car buying website. You can buy the car in online and see the car details. This app is built using
+React Native.
+
+
+## Screenshots
+### Mobile View
+![App Screenshot](https://i.ibb.co/QD4nHfx/car-native-app.jpg)
+
+### Tab View
+![App Screenshot](https://i.ibb.co/7ptZ9p4/car-native-app-tab.jpg)
+
+  
+## Features
+
+- React Props
+- Custom Component
+- Button Component
+- Render a list of items: FlatList
+- FlatList Snap effect
+
+  
+## Tech Stack
+
+React, Props, Render a list of items: FlatList, FlatList Snap effect
+
+
+
+  
+## Deployment
+
+To deploy this project run
+
+```bash
+  npm install
+```
+```bash
+  npm start
+```
+
+  
+## Contributor
+
+`Md. Azizul Haque Jany (Project Manager and Front End Developer)`
+
+`Emran H Khan - Front End Developer`
+
+`MD Rakibul Hassan - Front End Developer`
+
+  
---
+++ components/CarsList/cars.js
@@ -0,0 +1,53 @@
+//fake data
+export default [{
+    name: 'HUNTER S',
+    tagline: 'Starting at $69,420',
+    image: require('../../assets/images/ModelS.jpeg'),
+}, {
+    name: 'HUNTER 3',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Model3.jpeg'),
+}, {
+    name: 'HUNTER X',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelX.jpeg'),
+}, {
+    name: 'HUNTER Y',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelY.jpeg'),
+
+
+}, {
+    name: 'HUNTER E',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelE.jpeg'),
+
+
+}, {
+    name: 'HUNTER R',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelR.jpeg'),
+
+
+}, {
+    name: 'HUNTER J',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelJ.jpeg'),
+
+
+}, {
+    name: 'Model Z',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelZ.jpeg'),
+
+
+}
+
+];
